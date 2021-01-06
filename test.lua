3ffbd0910d44930fd5e27e626ce2d6482a5f331d
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-01-06 02:28:44 +0100

    add tests to delete method

--- test.lua
+++ test.lua
@@ -10,6 +10,8 @@ test('set', function(_assert)
     object.items,
     { ['name'] = 'Evandro', ['age'] = 34 }
   )
+
+  _assert.equal(object.size, 2)
 end)
 
 test('get', function(_assert)
@@ -21,4 +23,41 @@ test('has', function(_assert)
   _assert.ok(object.has('name'))
   _assert.ok(object.has('age'))
   _assert.not_ok(object.has('profession'))
+end)
+
+test('delete', function(_assert)
+  object.set('profession', 'Programmer')
+
+  _assert.deep_equal(
+    object.items,
+    {
+      ['name'] = 'Evandro',
+      ['age'] = 34,
+      ['profession'] = 'Programmer'
+    }
+  )
+
+  _assert.equal(object.size, 3)
+  _assert.ok(object.delete('profession'))
+  _assert.not_ok(object.has('profession'))
+  _assert.deep_equal(
+    object.items,
+    {
+      ['name'] = 'Evandro',
+      ['age'] = 34,
+    }
+  )
+
+  object.set('profession', 'Software Engineer')
+
+  _assert.ok(object.has('profession'))
+  _assert.equal(object.get('profession'), 'Software Engineer')
+  _assert.deep_equal(
+    object.items,
+    {
+      ['name'] = 'Evandro',
+      ['age'] = 34,
+      ['profession'] = 'Software Engineer'
+    }
+  )
 end)
